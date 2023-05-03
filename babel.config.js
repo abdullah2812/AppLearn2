2a381dacb151aafc00391a627f2038dfdd360032
Abdullah <iriandaabdullah546@gmail.com>
2023-05-03 19:53:05 +0700

    FixReanimated

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugin: [
-    'react-native-reanimated/plugin',
+  plugins: [
+    'react-native-reanimated/plugin',{
+    },
   ],
 };
